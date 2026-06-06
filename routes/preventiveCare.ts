2f5d5f5aa1798c24a240069a227006aedf04cf32
Alex Brown <brown.alex.k@gmail.com>
2026-06-05 21:55:51 -0600

    feat: improved code

--- routes/preventiveCare.ts
+++ routes/preventiveCare.ts
@@ -15,35 +15,32 @@ router.get('/types', (_req, res) => {
   res.json(CARE_TYPES);
 });
 
-router.get('/gaps', (_req, res) => {
-  const { typeid, overduemin, overduemax, page, limit } = _req.query;
+router.get('/gaps', (req, res) => {
+  const { typeid, overduemin, overduemax, page, limit } = req.query;
 
   const filteredById = typeid
     ? CARE_GAP_RECORDS.filter((record) => record.careGap.type === typeid)
     : CARE_GAP_RECORDS;
 
-  const filteredByMinMax = filteredById.filter((record) => {
-    const overdueDays = getOverdueDays(record.due);
-    return (
-      (overduemin === undefined || overdueDays >= Number(overduemin)) &&
-      (overduemax === undefined || overdueDays <= Number(overduemax))
-    );
-  });
-
   const reqPage = Math.max(1, parseInt(page as string) || 1);
   const reqLimit = Math.min(100, parseInt(limit as string) || 25);
   const offset = (reqPage - 1) * reqLimit;
 
-  const gaps = filteredByMinMax.map((record) => [
-    {
+  const gaps = filteredById
+    .map((record) => ({
       careGap: {
         ...record.careGap,
         name: careTypeMap.get(record.careGap.type),
       },
       patient: record.patient,
       daysOverdue: getOverdueDays(record.due),
-    },
-  ]);
+    }))
+    .filter(
+      (record) =>
+        (overduemin === undefined ||
+          record.daysOverdue >= Number(overduemin)) &&
+        (overduemax === undefined || record.daysOverdue <= Number(overduemax)),
+    );
 
   const paginatedGaps = gaps.slice(offset, offset + reqLimit);
 

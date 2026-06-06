2c8ed5199fc767d24edc00275ee05d0eeba81bd8
Alex Brown <brown.alex.k@gmail.com>
2026-06-06 13:06:38 -0700

    feat: created WidgetContainer component, updated App.tsx & updated Tailwind config

---
+++ client/src/components/WidgetContainer.tsx
@@ -0,0 +1,9 @@
+const WidgetContainer = () => {
+  return (
+    <section className="h-full w-full p-8">
+      <h2 className="text-2xl font-bold">Preventive Care Gaps</h2>
+    </section>
+  );
+};
+
+export default WidgetContainer;
